f88885b7a27adac4bf2ce2bd75b32bef33eadcda
Catalin <49250412+CataXOmi@users.noreply.github.com>
2022-06-08 16:28:32 +0300

    Changing Publisher to send custom messages+storebu

--- samples/snippets/quickstart/pub.py
+++ samples/snippets/quickstart/pub.py
@@ -17,9 +17,11 @@
 import argparse
 
 from google.cloud import pubsub_v1
+from google.cloud import storage
+from datetime import datetime
 
 
-def pub(project_id: str, topic_id: str) -> None:
+def pub(project_id: str, topic_id: str, mesaj: str) -> None:
     """Publishes a message to a Pub/Sub topic."""
     # Initialize a Publisher client.
     client = pubsub_v1.PublisherClient()
@@ -27,7 +29,15 @@ def pub(project_id: str, topic_id: str) -> None:
     topic_path = client.topic_path(project_id, topic_id)
 
     # Data sent to Cloud Pub/Sub must be a bytestring.
-    data = b"Hello, World!"
+    data = bytes(mesaj, 'UTF-8')
+
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
+    
+    stor_client = storage.Client()
+    bucket = stor_client.get_bucket('pubsubcc')
+    blob = bucket.blob('mesaje' + dt_string)
+    blob.upload_from_string(data)
 
     # When you publish a message, the client returns a future.
     api_future = client.publish(topic_path, data)
@@ -42,7 +52,8 @@ if __name__ == "__main__":
     )
     parser.add_argument("project_id", help="Google Cloud project ID")
     parser.add_argument("topic_id", help="Pub/Sub topic ID")
-
+    parser.add_argument("mesaj", help="Mesaj custom")
+    
     args = parser.parse_args()
 
-    pub(args.project_id, args.topic_id)
+    pub(args.project_id, args.topic_id, args.mesaj)
